30c58332ae6327f2d9d071f90b9661f891e0c64d
Carlos Loureda <balanzeneto@gmail.com>
2019-10-27 13:56:33 +0100

    design: Added personal logo and favicon :)

--- src/components/HomeBanner/nav/Nav.js
+++ src/components/HomeBanner/nav/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-import logo from "../../../img/logo.png";
+// import logo from "../../../img/logo.png";
+import logo from "../../../img/carlos_loureda.png";
 
 import styled from "styled-components";
 
